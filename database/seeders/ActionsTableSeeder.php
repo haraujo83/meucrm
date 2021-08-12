936d03d04cd4873d17761780cb9747bc4697ae51
Mateus Guizelini <mateus.guizelini@tech.math.group>
2021-08-12 14:38:51 -0300

    Modificando seeder

--- database/seeders/ActionsTableSeeder.php
+++ database/seeders/ActionsTableSeeder.php
@@ -16,12 +16,12 @@ class ActionsTableSeeder extends Seeder
     {
         DB::table('actions')->insert([
             ['id' => 1, 'text' => 'Listar', 'action' => 'index'],
-            ['id' => 2, 'text' => 'Incluir', 'action' => 'create'],
+            ['id' => 2, 'text' => 'Criar', 'action' => 'create'],
             ['id' => 3, 'text' => 'Alterar', 'action' => 'edit'],
             ['id' => 4, 'text' => 'Excluir', 'action' => 'destroy'],
-            ['id' => 5, 'text' => 'Incluir Financiamento', 'action' => 'create_financing'],
-            ['id' => 6, 'text' => 'Incluir Home', 'action' => 'create_homeequity'],
-            ['id' => 7, 'text' => 'Incluir Consórcio', 'action' => 'create_consortium'],
+            ['id' => 5, 'text' => 'Criar Financiamento', 'action' => 'create_financing'],
+            ['id' => 6, 'text' => 'Criar Home', 'action' => 'create_homeequity'],
+            ['id' => 7, 'text' => 'Criar Consórcio', 'action' => 'create_consortium'],
         ]);
     }
 }
