a874aba06e2d9a83a31d78b9493da4013e682e8f
Mateus Guizelini <mateus.guizelini@tech.math.group>
2021-08-16 17:16:51 -0300

    Merge model account

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Model para tabela accounts
+ */
 class Account extends Model
 {
     use HasFactory;
+
+    public $table = 'accounts';
+    public $fillable = [
+        'name', 'date_entered', 'date_modified', 'modified_user_id', 'created_by', 'description',
+        'deleted', 'assigned_user_id', 'account_type', 'billing_address_street', 'billing_address_city',
+        'billing_address_state', 'billing_address_postalcode', 'billing_address_country', 'website',
+        'parent_id', 'cnpj', 'razao_social', 'billing_address_number', 'billing_address_bairro',
+        'account_status', 'porte', 'account_situacao', 'codigo_siav', 'lead_source', 'frequencia',
+        'perfil_conta', 'frequencia_lead', 'frequencia_venda', 'consultor_prosp_id', 'codigo_sap',
+        'date_followup', 'rating_conta', 'termometro', 'motivo', 'crediline_user', 'crediline_pass'
+    ];
+
+    public $timestamps = false;
+
+    /**
+     * Retorna o registro de lead
+     * @return BelongsTo
+     */
+    public function lead(): BelongsTo
+    {
+        return $this->belongsTo(Leads::class, 'id', 'account_id');
+    }
 }
