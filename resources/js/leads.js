b66f85defca1c503cb6d26f7aee23a90ced5e984
Helder Araujo <helder.araujo@tech.math.group>
2021-08-18 16:57:00 -0300

    busca account p/ busca lead (melhorias)

--- resources/js/leads.js
+++ resources/js/leads.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
             dataType: 'json',
             delay: 250,
             minimumInputLength: 3,
+            transport: function (params, success) {
+                if (typeof params.data.term === 'string' && params.data.term.length >= params.minimumInputLength) {
+                    const $request = $.getJSON(params);
+
+                    $request.then(success);
+
+                    $request.fail(function (xhr) {
+                        app.err(xhr.responseJSON.message);
+                    });
+
+                    return $request;
+                }
+            },
             processResults: function (data) {
                 return {
                     results: $.map(data, function (v, i) {
