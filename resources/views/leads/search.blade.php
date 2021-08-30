628d166558931cccce1476f8a67bae97098db52e
Mateus Guizelini <mateus.guizelini@tech.math.group>
2021-08-30 18:42:57 -0300

    Ajuste na busca

--- resources/views/leads/search.blade.php
+++ resources/views/leads/search.blade.php
@@ -30,17 +30,7 @@
         {{ Form::bsDateRange('periodo_criacao', 'Período de Criação:', $filters['periodo_criacao'] ?? null) }}
     </div>
     <!--/.row -->
-    <div class="row mb-1">
-        <div class="col-md-2">
-            <a href="{{route('fields_search.moduleResultColumnsIndex')}}?module={{$module}}" class="btn btn-default btn-xs"
-               data-tippy-content="Selecionar colunas a serem exibidas no Resultado"
-               data-module="{{$module}}"
-               id="select-result-cols"
-            >
-                <i class="fas fa-columns"></i> Selecionar Colunas
-            </a>
-        </div>
-    </div>
+    @include('selectColumns')
     <!--/.row -->
     <div class="row">
         <div class="col-md-12">
